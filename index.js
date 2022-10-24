d4f095f185f796896571585918c4ba6581bf54f5
Cade Duncan <cadeduncan72@gmail.com>
2022-10-23 23:42:48 -0700

    Progress on project panels

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ OverlayScrollbars(document.querySelectorAll("body"), {} );
 document.addEventListener("DOMContentLoaded", function() {
   //The first argument are the elements to which the plugin shall be initialized
 	//The second argument has to be at least a empty object or a object with your desired options
-  OverlayScrollbars(document.querySelectorAll("body"), {defaultOptions});
+  OverlayScrollbars(document.querySelectorAll("body"), {className : "os-theme-light"});
+
+  $(document).ready(function () { $('body').hide().fadeIn(1500).delay(10000)});
 });
